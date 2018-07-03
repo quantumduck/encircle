8ceef0cdb74b78fbc75e16271a257ff42d6061d2
quantumduck <quantum.duck@hotmail.com>
2018-07-03 16:27:14 -0400

    main components

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ function isFunction(string) {
 }
 
 function calculateExpression() {
+  clearError();
   var result = calculate(document.getElementById('sExpression').value);
   document.getElementById('message-box').innerHTML = `=&gt; ${result}`;
 }
@@ -21,7 +22,22 @@ function calculate(string) {
   if (isInteger(string)) {
     return parseInt(string, 10);
   } else if (isFunction(string)) {
-    return 42;
+    var params = getFunctionParams(trimEnds(string));
+    if (params.length === 3) {
+      if (params[0] === "add") {
+        return calculate(params[1]) + calculate(params[2]);
+      } else if (params[0] === "multiply") {
+        return calculate(params[1]) * calculate(params[2]);
+      } else {
+        printError(`Unknown function: ${params[0]}`);
+        return NaN;
+      }
+    } else if (params.length > 0) {
+      printError(`Wrong number of parameters in: ${string}`);
+      return NaN;
+    } else {
+      return NaN;
+    }
   } else {
     printError(`Unparseable expression: ${string}`);
     return NaN;
@@ -30,6 +46,33 @@ function calculate(string) {
 
 function trimEnds(string) {
   return string.substring(1, string.length - 1);
+}
+
+function getFunctionParams(string) {
+  var params = [];
+  var parenCount = 0;
+  var startIndex = 0;
+  for (var i = 0; i < string.length; i++) {
+    var char = string[i];
+    if (char === '(') {
+      parenCount += 1;
+    } else if (char === ')') {
+      parenCount -= 1;
+    } else if (char === ' ' && parenCount === 0) {
+      params.push(string.substring(startIndex, i));
+      startIndex = i + 1;
+    }
+    if (parenCount < 0) {
+      printError('Unmatched close param found');
+      return [];
+    }
+  }
+  if (parenCount > 0) {
+    printError('Unmatched open param found');
+    return [];
+  }
+  params.push(string.substring(startIndex));
+  return params;
 }
 
 function printError(message) {
